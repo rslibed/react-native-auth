74ce03630335884d21d9c75049c812e38870d31e
rslibed <rslibed@gmail.com>
2018-05-11 23:29:49 -0700

    View of login form or logout button is rendered depending on if user is logged in/out

--- src/App.js
+++ src/App.js
@@ -2,18 +2,36 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 import firebaseCreds from '../creds';
-import { Header } from './components/common';
+import { Header, Button, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: false };
   componentWillMount() {
     firebase.initializeApp(firebaseCreds.keys);
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+  logUserOut() {
+    this.setState({ loggedIn: false });
   }
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
+        { this.state.loggedIn ?
+        <CardSection> 
+          <Button onPress={this.logUserOut.bind(this)}>
+            Log Out
+          </Button>
+        </CardSection> :
         <LoginForm />
+        }
       </View>
     );
   }
